5a2bc82110445343d98eb49f9062079afe08f55f
nayaracalenzo <queiroz.14@hotmail.com>
2025-11-26 21:21:20 -0300

    sql com consultas e filtros

--- db_fs05_26_11_25.sql
+++ db_fs05_26_11_25.sql
@@ -32,8 +32,78 @@ ADD FOREIGN KEY (departamento_id) REFERENCES departamentos(id);
 
 -- DROP TABLE funcionarios; (para deletar a tabela)
 
+
+
 -- 1. CRIAR TABELA CARGOS (id, nome)
+CREATE TABLE cargos (
+id SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL
+);
+
 -- 2. DROP COLUNA cargo da tabela funcionarios
+
+ALTER TABLE funcionarios DROP COLUMN cargo;
+
 -- 3. ALTERAR TABELA funcionarios add coluna cargo_id e constraint FK 
+
+ALTER TABLE funcionarios ADD COLUMN cargo_id INT, 
+ADD FOREIGN KEY (cargo_id) REFERENCES cargos(id);
+
+
+-- Inserir dados na tabela 
+
+INSERT INTO cargos (nome) VALUES ('psicologo'), ('professor'), ('atendente'); 
+
+SELECT * FROM cargos;
+
+
+INSERT INTO departamentos (nome) VALUES ('Recursos Humanos'), ('Financeiro'),
+('Atendimento ao Cliente'), ('Acadêmico');
+
+SELECT * FROM departamentos;
+
+-- Deletando dados na tabela
+
+-- DELETE FROM departamentos WHERE nome = 'Recursos Humanos';
+DELETE FROM departamentos WHERE id = 1;
+
+
+INSERT INTO funcionarios (nome, salario, email, departamento_id, cargo_id)
+VALUES ('Rodrigo Medeiros', 10000.00, 'rodrigo@mail.com', 4, 2);
+
+INSERT INTO funcionarios (nome, salario, email, departamento_id, cargo_id)
+VALUES ('Nayara Calenzo', 5000.00, 'nayara@mail.com', 4, 2), 
+('Vanessa', 2500.00, 'vanessa@mail.com', 3, 3),
+('Sarah', 3000.00, 'sarah@mail.com', 2, 3);
+
+SELECT * FROM funcionarios;
+
+
+-- selects com filtro (WHERE)
+SELECT * FROM funcionarios WHERE salario > 3000;
+
+SELECT * FROM funcionarios WHERE email = 'nayara@mail.com';
+
+-- select com filtro (LIKE) 
+-- % antes (termina com..)
+-- % depois (começa com..)
+-- % antes e depois (pode começar ou terminar com..)
+SELECT * FROM funcionarios WHERE email LIKE '%nayara%';
+
+-- alterando dados da tabela
+UPDATE funcionarios SET email = 'nayara@email.com' WHERE id = 2;
+
+-- select com ordenação (ORDER BY - DESC/ASC) 
+SELECT * FROM funcionarios ORDER BY salario DESC;
+
+SELECT nome, salario FROM funcionarios ORDER BY salario ASC;
+
+
+-- como renomear uma coluna (AS) 
+SELECT nome AS nome_usuario FROM funcionarios WHERE ativo = true;
+
+
+
+
 
 
